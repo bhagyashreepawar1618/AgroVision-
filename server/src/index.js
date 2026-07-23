84ce5c1b15ac7d5283ab185fd264e25d059e954f
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-07-23 23:38:24 +0530

    basic sever req get and listen

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import app from "./app.js";
+
+app.listen(5000, () => {
+  console.log("server is listening to the port ", process.env.PORT);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hhelloo pawar you are on server</h1>");
+});
