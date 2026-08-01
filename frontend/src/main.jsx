bde78c5ff1da82096d2e55cff6eb9baa3033e085
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-01 19:06:01 +0530

    done with routing using react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,33 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true, // Home route
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={route} />,
 );
