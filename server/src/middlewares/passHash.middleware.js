637ef19a19f06b0a51b88238b83baf13fc36148d
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-07 21:18:34 +0530

    bcrypt middleare updated

--- server/src/middlewares/passHash.middleware.js
+++ server/src/middlewares/passHash.middleware.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 
 const hashpassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("You are in hash pass");
 
   if (!hashedPassword) {
     console.log("Password is not hashed");
