5bb8b5bff8229bffbf3d25dcd8927dd48efcdd6b
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-07 17:12:45 +0530

    created bcrypt middleware to hash the password

---
+++ server/src/middlewares/passHash.middleware.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+import ApiError from "../utils/ApiError.js";
+
+const hashpassword = async (password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  if (!hashedPassword) {
+    console.log("Password is not hashed");
+    throw new ApiError(500, "Something went wrong while hashing the password");
+  }
+
+  return hashedPassword;
+};
+
+export default hashpassword;
