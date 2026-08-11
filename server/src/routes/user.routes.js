a7c82a8c99b1655fedc6e965855c4d5290e4de76
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-11 19:19:57 +0530

    added route for login user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
+import { LoginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(upload.single("profile"), registerUser);
+router.route("/login").post(LoginUser);
 
 export default router;
