e80288b9acb7b641e50d266ccea005e3f5e8828c
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-12 11:37:51 +0530

    added userProfile route using react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import DiseasePrediction from "./components/Features/DiseasePrediction.jsx";
 import IrrigationPlanner from "./components/Features/IrrigationPlanner.jsx";
 import CropHistory from "./components/Features/CropHistory.jsx";
 import AiChatbot from "./components/Features/AiChatbot.jsx";
+import UserProfile from "./components/UserProfile.jsx";
 
 const route = createBrowserRouter([
   {
@@ -63,6 +64,10 @@ const route = createBrowserRouter([
       {
         path: "ai_chatbot",
         element: <AiChatbot />,
+      },
+      {
+        path: "user_profile",
+        element: <UserProfile />,
       },
     ],
   },
