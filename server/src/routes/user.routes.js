8f98f7aafdc208884e237dfce40dc30d82951586
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-12 11:36:07 +0530

    added route to get user profile

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { LoginUser, registerUser } from "../controllers/user.controller.js";
+import { getUserProfile, LoginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/jwt.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(upload.single("profile"), registerUser);
 router.route("/login").post(LoginUser);
+router.route("/profile").get(verifyJwt, getUserProfile);
 
 export default router;
