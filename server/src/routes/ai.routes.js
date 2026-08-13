02c6386893b3bc43c085178846d2a443f4e15f0c
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-13 16:24:46 +0530

    added route for ai suggestion

--- server/src/routes/ai.routes.js
+++ server/src/routes/ai.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { aiChatBot } from "../controllers/ai.controllers.js";
+import { aiChatBot, aiWeatherSuggestions } from "../controllers/ai.controllers.js";
 import { verifyJwt } from "../middlewares/jwt.middleware.js";
 
 const airouter = Router();
 
 //secured route
 airouter.route("/aichatbot").post(verifyJwt, aiChatBot);
-
+airouter.route("/weather-suggestion").post(verifyJwt, aiWeatherSuggestions);
 export default airouter;
