58e4c7b89f1e91a7c30a17f53f525c225500e3ca
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-16 23:32:11 +0530

    updated file

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -1,6 +1,7 @@
-import openAI, { OpenAI } from "openai";
+import { OpenAI } from "openai";
 
 const openai = new OpenAI({
+  baseURL: "https://router.huggingface.co/v1",
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
