e791272fba2525200cced58b503adeabc337f14e
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-08-19 23:11:30 +0530

    updated jwt middleware validation added for accesstoken

--- server/src/middlewares/jwt.middleware.js
+++ server/src/middlewares/jwt.middleware.js
@@ -29,9 +29,11 @@ export const generateAccessToken = async (username, email) => {
     }
   );
 };
+
 export const verifyJwt = async (req, _, next) => {
   try {
     const token = req.header("Authorization")?.replace("Bearer ", "");
+
     console.log("access token is=", token);
 
     if (!token) {
@@ -40,7 +42,20 @@ export const verifyJwt = async (req, _, next) => {
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    console.log("decoded token =", decodedToken);
+    //fetch token stored in db at the time of login
+    //check if the token from user is same as token stored in db
+    const user = await prisma.user.findFirst({
+      where: {
+        username: decodedToken.username,
+      },
+    });
+
+    const tokenFromDb = user.accessToken;
+
+    //check if both are same
+    if (tokenFromDb != token) {
+      throw new ApiError(400, "Invalid accesstoken recieved");
+    }
 
     req.user = decodedToken;
 
